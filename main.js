abc7c803f0afed2a17c1b5b59ab3c34cd8d33fbc
Damdadira <suk971029@gmail.com>
2022-07-11 15:45:42 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector('#navbar'); //CSS에서 불러오겠다 '#navbar라는 항목을'
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 //navbar 투명하게 만들건데 올라가면 투명, 내려오면 색상 출력
@@ -12,4 +12,18 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+//navbar에서 메뉴 선택했을 때 해당 페이지로 이동하도록 설정
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
