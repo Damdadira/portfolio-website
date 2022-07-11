2d3b64dabff850943cddb1c1256dd43a0248a5a8
Damdadira <suk971029@gmail.com>
2022-07-11 15:56:01 +0900

    JS: Home - contact button

--- main.js
+++ main.js
@@ -24,6 +24,17 @@ navbarMenu.addEventListener('click', (event) => {
     return;
   }
   console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+  scrollIntoView(link);
 });
+
+//Home의 Contact me를 누르면 Contact 페이지로 이동하도록
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
