f3bc330b93539850057f2872587478499622cb86
Damdadira <suk971029@gmail.com>
2022-07-12 18:25:43 +0900

    JS: Nav Scroll bug modification

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ navbarMenu.addEventListener('click', (event) => {
   }
   navbarMenu.classList.remove('open');
   scrollIntoView(link);
+  selectNavItem(target); //수동적으로 선택할 수 있도록(상단바 선택할 때 버그가 발생할 수도 있기 때문)
 });
 
 //small screen에서 navbar toggle button 작동하게
@@ -107,6 +108,7 @@ function scrollIntoView(selector) {
 
 // 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
 const sectionIds = [
+  //우리가 사용하는 모든 아이디를 배열로(문자열로) 저장
   '#home',
   '#about',
   '#skills',
@@ -114,18 +116,19 @@ const sectionIds = [
   '#testimonials',
   '#contact',
 ];
-const sections = sectionIds.map((id) => document.querySelector(id));
-const navItems = sectionIds.map((id) =>
-  document.querySelector(`[data-link="${id}"]`)
+const sections = sectionIds.map((id) => document.querySelector(id)); //모든 sectionIds 요소들을 sections에 할당
+const navItems = sectionIds.map(
+  (id) => document.querySelector(`[data-link="${id}"]`) //동일한 네비게이션 아이템 요소들을 섹션 navItems에 할당
 );
 // console.log(sections); ->값 확인용
 // console.log(navItems);
 
 // 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+//현재 선택된 메뉴 인덱스와 메뉴 요소들을 변수에 저장
 let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
-
-function selectedNavItem(selected) {
+//새로운 메뉴 아이템을 선택할 때마다 이전에 활성화된 아이는 지워주고, 다시 새롭게 할당하고 나서 active를 지정
+function selectNavItem(selected) {
   selectedNavItem.classList.remove('active');
   selectedNavItem = selected;
   selectedNavItem.classList.add('active');
@@ -137,6 +140,7 @@ const observerOptions = {
   threshold: 0.3,
 };
 
+//IntersectionObserver를 이용해서 섹션에 밖으로 나갈 때마다 그 다음에 해당하는 인덱스를 계산
 const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
     // console.log(entry.target); ->값 확인용
@@ -144,6 +148,7 @@ const observerCallback = (entries, observer) => {
       const index = sectionIds.indexOf(`#${entry.target.id}`);
       // console.log(index, entry.target.id); ->값 확인용
 
+      //스크롤이 중간에 있다면
       //스크롤링이 아래로 되어서 페이지가 올라옴
       if (entry.boundingClientRect.y < 0) {
         selectedNavIndex = index + 1;
@@ -156,3 +161,22 @@ const observerCallback = (entries, observer) => {
 };
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach((section) => observer.observe(section));
+
+//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+window.addEventListener('wheel', () => {
+  //'wheel'->사용자가 직접 상단바 선택할 때만, 'scroll'->마우스로 스크롤 할 때
+  if (window.scrollY === 0) {
+    //스크롤이 제일 위에 있다면 'Home'을 선택하도록
+    selectedNavIndex = 0;
+  } else if (
+    //scrollY와 window창의 innerHeight값을 더한값이 document.body.clientHeight값과
+    //정확하게 일치하지 않는 경우가 있기 때문에 더한값을 반올림 해줘야 함
+    //ex->(scrollY+innerHeignt = 1269.2) != (document.body.clientHeight = 1270)
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight
+  ) {
+    //스크롤이 제일 밑에 도달했다면 'Contact'를 선택하도록
+    selectedNavIndex = navItems.length - 1; //제일 마지막 인덱스를 가리켜라
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
